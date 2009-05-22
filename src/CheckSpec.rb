7bda55f69d64c2648a133d8727bca5dbbc9dadc5
rennhak <github@thewebagency.org>
2009-05-22 16:29:02 +0900

    - Started the CheckSpec.rb coding.

---
+++ src/CheckSpec.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby -w
+#
+
+###
+#
+# File: CheckSpec.rb
+#
+# (c) 2009, Bjoern Rennhak, The University of Tokyo
+# CheckSpec.rb reads the ../specification/XYAMLSpecification.yaml file and provides a CLI interface to
+# either output the specific problem or only a binary switch if the spec is ok (TRUE, 1) or not ok
+# (FALSE, 0).
+#
+# This tool is normally used by developers who extend the XYAMLSpecification.yaml file not by
+# end-users.
+#
+#########
+
+
+
