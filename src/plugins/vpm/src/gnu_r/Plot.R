810e0fc86378c0addf5e0f1a1b4926c4709e6b2e
rennhak <github@thewebagency.org>
2009-08-19 07:07:55 +0900

    - Finished first evaluation of body components method, looks promising

--- src/plugins/vpm/src/gnu_r/Plot.R
+++ src/plugins/vpm/src/gnu_r/Plot.R
@@ -13,7 +13,7 @@ points( x1, y1, pch = 19, col = "green", cex = 1.5)
 
 results.spl <- sm.spline(results$x, results$y)
 #results.spl <- smooth.Pspline(results$x, results$y, method = 3)
-lines(results.spl, col = "blue")
+#lines(results.spl, col = "blue")
 lines(sm.spline(results$x, results$y, df=10), lty=1, col = "red")
 
 
